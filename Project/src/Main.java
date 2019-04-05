8a6d7bf9c9f577e3aea4fa3ff607ad437088be05
Jin-Woo Kim <dnehd6739@naver.com>
2019-04-05 23:34:31 +0900

    heap lib add

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -5,6 +6,7 @@ public class Main {
 	static long start,end;
 	static int[] Random = new int[100000];
 	static int[] Reverse = new int[100000];
+	static int size=0;
 	
 	
 	
@@ -15,10 +17,12 @@ public class Main {
 		System.out.println("Selection"+"	"+selectionsort(Random_array(1000))+"	"+selectionsort(Reverse_array(1000))+"	"+selectionsort(Random_array(10000))+"	"+selectionsort(Reverse_array(10000))+"	"+selectionsort(Random_array(100000))+"	"+selectionsort(Reverse_array(100000)));
 		System.out.println("Insertion"+"	"+insertionsort(Random_array(1000))+"	"+insertionsort(Reverse_array(1000))+"	"+insertionsort(Random_array(10000))+"	"+insertionsort(Reverse_array(10000))+"	"+insertionsort(Random_array(100000))+"	"+insertionsort(Reverse_array(100000)));
 		System.out.println("Merge"+"		"+mergesort(Random_array(1000),0,999)+"	"+mergesort(Reverse_array(1000),0,999)+"	"+mergesort(Random_array(10000),0,9999)+"	"+mergesort(Reverse_array(10000),0,9999)+"	"+mergesort(Random_array(100000),0,99999)+"	"+mergesort(Reverse_array(100000),0,99999));
-		System.out.println("Quick1"+"	"+Quicksort1(Random_array(1000),0,999)+"	"+Quicksort1(Reverse_array(1000),0,999)+"	"+Quicksort1(Random_array(10000),0,9999)+"	"+Quicksort1(Reverse_array(10000),0,9999)+"	"+Quicksort1(Random_array(100000),0,99999)+"	"+Quicksort1(Reverse_array(100000),0,99999));
-		System.out.println("Quick2"+"	"+Quicksort2(Random_array(1000),0,999)+"	"+Quicksort2(Reverse_array(1000),0,999)+"	"+Quicksort2(Random_array(10000),0,9999)+"	"+Quicksort2(Reverse_array(10000),0,9999)+"	"+Quicksort2(Random_array(100000),0,99999)+"	"+Quicksort2(Reverse_array(100000),0,99999));
-		System.out.println("Quick3"+"	"+Quicksort3(Random_array(1000),0,999)+"	"+Quicksort3(Reverse_array(1000),0,999)+"	"+Quicksort3(Random_array(10000),0,9999)+"	"+Quicksort3(Reverse_array(10000),0,9999)+"		"+Quicksort3(Random_array(100000),0,99999)+"	"+Quicksort3(Reverse_array(100000),0,99999));
-	}
+//		System.out.println("Quick1"+"		"+Quicksort1(0,999,Random_array(1000))+"	"+Quicksort1(0,999,Reverse_array(1000))+"	"+Quicksort1(0,9999,Random_array(10000))+"	"+Quicksort1(0,9999,Reverse_array(10000))+"	"+Quicksort1(0,99999,Random_array(100000))+"   "+Quicksort1(0,99999,Reverse_array(100000)));		
+		System.out.println("Quick2"+"		"+Quicksort2(Random_array(1000),0,999)+"	"+Quicksort2(Reverse_array(1000),0,999)+"	"+Quicksort2(Random_array(10000),0,9999)+"	"+Quicksort2(Reverse_array(10000),0,9999)+"	"+Quicksort2(Random_array(100000),0,99999)+"   "+Quicksort2(Reverse_array(100000),0,99999));
+		System.out.println("Quick3"+"		"+Quicksort3(Random_array(1000),0,999)+"	"+Quicksort3(Reverse_array(1000),0,999)+"	"+Quicksort3(Random_array(10000),0,9999)+"	"+Quicksort3(Reverse_array(10000),0,9999)+"	"+Quicksort3(Random_array(100000),0,99999)+"   "+Quicksort3(Reverse_array(100000),0,99999));
+		System.out.println("heap"+"		"+Heapsort(Random_array(1000))+"	"+Heapsort(Reverse_array(1000))+"	"+Heapsort(Random_array(10000))+"	"+Heapsort(Reverse_array(10000))+"	"+Heapsort(Random_array(100000))+"   "+Heapsort(Reverse_array(100000)));
+		System.out.println("lib"+"		"+api(Random_array(1000))+"	"+api(Reverse_array(1000))+"	"+api(Random_array(10000))+"	"+api(Reverse_array(10000))+"	"+api(Random_array(100000))+"   "+api(Reverse_array(100000)));
+	}	
 	
 	public static int[] Random_array(int data) {
 		Random=new int[data];
@@ -27,6 +31,7 @@ public class Main {
 		}
 		return Random;
 	}
+	
 	public static int[] Reverse_array(int data) {
 		int index = data;
 		Reverse=new int[data];
@@ -35,6 +40,7 @@ public class Main {
 		}
 		return Reverse;
 	}
+	
 	public static double bubblesort(int[] array) {
 		start = System.currentTimeMillis();
 		for(int i = 0 ; i < array.length ; i ++) {
@@ -109,19 +115,20 @@ public class Main {
 		for(int a=p;a<k;a++)
 				array[a] = temp[a];
 	}
-	public static double Quicksort1(int[] array,int p,int r) {
+	
+	public static double Quicksort1(int p,int r,int array[]) {
 		start = System.currentTimeMillis();
-		int q=0;
 			if(p<r) {
-				q = lastpivot(array,p,r); //pivot위치
-				Quicksort1(array,p,q-1);
-				Quicksort1(array,q+1,r);	
+				int q = lastpivot(p,r,array); //pivot위치
+				Quicksort1(p,q-1,array);
+				Quicksort1(q+1,r,array);	
 			}
 		 end = System.currentTimeMillis();
 		
-			return ((double)end-start)/1000;
+		return ((double)end-start)/1000;
 	}
-	public static int lastpivot(int[] array,int p,int r) {
+	
+	public static int lastpivot(int p,int r,int array[]) {
 		int x = array[r];
 		int i = p-1;
 		for(int j=p;j<r;j++) { 
@@ -133,6 +140,7 @@ public class Main {
 		swap(array,i+1,r);
 		return i+1;
 	}
+	
 	public static double Quicksort2(int[] array,int p,int r) {
 		start = System.currentTimeMillis();//가운데값
 		if(p<r) {
@@ -145,17 +153,18 @@ public class Main {
 		return ((double)end-start)/1000;
 	}
 	public static int middlepivot(int[] array,int p,int r) {
-		int pivot = array[(p +  r) / 2];
-		while (p <  r) {
-			while ((array[p] < pivot) && (p <  r))
-				 p++;
-			while ((array[ r] > pivot) && (p <  r))
-				 r--;
-			if (p <  r) {
-				swap(array,p,r);
+		int mid = (p+r)/2;
+		int pv = array[mid];
+		swap(array, mid, r-1);
+		int i = p - 1;
+		for(int j = p; j < r; j++) {
+			if(array[j] <= pv) {
+				i++;
+				swap(array, i, j);
 			}
 		}
-		return p;
+		swap(array, i+1, r);
+		return i+1;
 	}
 	public static double Quicksort3(int[] array,int p,int r) {
 		start = System.currentTimeMillis();//랜덤
@@ -192,11 +201,56 @@ public class Main {
         return pivotIndex;
     }
 
-    public static void swap(int[] array, int firstIndex, int secondIndex) {
-        int temp = array[firstIndex];
-        array[firstIndex] = array[secondIndex];
-        array[secondIndex] = temp;
+    public static void swap(int[] array,int p,int r) {
+        int temp = array[p];
+        array[p] = array[r];
+        array[r] = temp;
     }
+public static void MAX_HEAPIFY(int k, int array[]) {
+		
+		while(k*2+1<=size) 
+		{
+			int j=k*2+1;
+			if(k*2+2<=size)
+			{
+				if(array[k*2+1]<array[k*2+2])
+					j=k*2+2;
+			}
+			if(array[k]<array[j])
+				swap(array,k,j);
+			else
+				return ;
+			k = j;
+		}
+	}
+	public static void Heap(int array[]) {
+		int i=(array.length-1)/2;
+		while(i>=0) {
+			MAX_HEAPIFY(i,array);
+			i--;
+		}
+	}
+	public static double Heapsort(int array[])
+	{
+		start = System.currentTimeMillis();
+		size=array.length-1;
+		Heap(array);
+		for(int i=size;i>=1;i--) {
+			swap(array,i,0);
+			size--;
+			MAX_HEAPIFY(0,array);
+		}
+		end = System.currentTimeMillis();
+		
+		return ((double)end-start)/1000;
+	}
+	public static double api(int array[]) {
+		start = System.currentTimeMillis();
+		Arrays.sort(array);
+		end = System.currentTimeMillis();
+		
+		return ((double)end-start)/1000;
+	}
 	
 }
 
